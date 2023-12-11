14aed8e3cbe6047744cf0b9efc1dc18795e547e0
Noman Hasan <NomanSepaks@gmail.com>
2023-12-11 23:53:41 +0600

    form handle

--- fifth_project/first_app/views.py
+++ fifth_project/first_app/views.py
@@ -5,6 +5,13 @@ def home(request):
     return render(request,'./first_app/home.html',)
 
 def about(request):
-    return render(request,'./first_app/about.html',)
+     if request.method =='POST':
+        name = request.POST.get('username') 
+        email = request.POST.get('email') 
+        return render(request,'./first_app/about.html', {'name':name, 'email': email}) 
+     else:  
+        return render(request,'./first_app/about.html',)
+
 def submit_form(request):
-    return render(request,'./first_app/form.html',)
+   
+     return render(request,'./first_app/form.html',)
