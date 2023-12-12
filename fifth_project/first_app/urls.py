872d29c4c32a1b6d30f6c227d0bbb6b9919fcfd0
Noman Hasan <NomanSepaks@gmail.com>
2023-12-12 23:54:41 +0600

    form handle updated

--- fifth_project/first_app/urls.py
+++ fifth_project/first_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='homepage',),
     path('about/', views.about, name='aboutpage',),
     path('form/', views.submit_form, name='submit_form',),
+    path('django_form/', views.Django_form, name='django_form',),
     # path('about/', views.about),
 ]
